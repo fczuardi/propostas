2b60d5d5c3208a012f43585eb466c72d8b52dde0
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-07 11:10:10 -0300

    generate PDF from template + env vars

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" --harmony_destructuring --harmony_sloppy_let "$0" "$@"
+require('shelljs/global');
+
+const contents = cat('template.md');
+const pattern = /___(.*?)___/g;
+const placeholders = contents.match(pattern);
+
+cp('template.md', 'p1.md');
+placeholders.forEach( (placeholder) => {
+    let varname = placeholder.replace(pattern, '$1');
+    let value = env[varname];
+    sed('-i', placeholder, value, 'p1.md');
+});
+
+
+echo(cat('p1.md'));
